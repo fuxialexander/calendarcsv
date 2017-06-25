ccb38478020a31c2b11d194ca61df3e851da25a6
fuxialexander <fuxialexander@users.noreply.github.com>
2017-06-26 00:23:00 +0800

    Create main.swift

---
+++ Sources/main.swift
@@ -0,0 +1,49 @@
+import EventKit
+
+let eventStore = EKEventStore()
+let daysToSynchronize = Int(CommandLine.arguments[1])
+let datefor = DateFormatter()
+
+datefor.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+func checkCalendarAuthorizationStatus() {
+    let status = EKEventStore.authorizationStatus(for: EKEntityType.event)
+    
+    switch (status) {
+    case EKAuthorizationStatus.notDetermined:
+        
+        requestAccessToCalendar()
+        
+    case EKAuthorizationStatus.authorized:
+        
+        print("Done.")
+        
+        
+    case EKAuthorizationStatus.restricted, EKAuthorizationStatus.denied:
+        // We need to help them give us permission
+        print("Denied")
+    }
+}
+
+func requestAccessToCalendar() {
+    eventStore.requestAccess(to: EKEntityType.event, completion: {
+        (accessGranted: Bool, error: Error?) in
+        
+        if accessGranted == true {
+            print("Granted")
+        } else {
+            print("Need permissions")
+        }
+    })
+}
+
+checkCalendarAuthorizationStatus()
+
+eventStore.enumerateEvents(matching:eventStore.predicateForEvents(withStart: Date().addingTimeInterval(TimeInterval(-1*daysToSynchronize*60*60*24)), end: Date(), calendars: nil), using:{
+    (event:EKEvent, stop:UnsafeMutablePointer<ObjCBool>) in
+    
+    print(datefor.string(from: event.startDate), datefor.string(from: event.endDate), event.title, event.calendar.title, separator: "\t")
+    
+})
+
+
